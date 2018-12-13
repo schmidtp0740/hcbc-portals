9cc152adc7bb01aeec23da3c4629174fb3b17d8f
schmidtp0740 <schmidtp0740@gmail.com>
2018-12-12 19:06:26 -0600

    adding ledger

--- src/actions/fetchUrls.js
+++ src/actions/fetchUrls.js
@@ -2,3 +2,5 @@ const url = "http://medbo-backend.jx-staging.129.213.177.165.nip.io"
 export const rxUrl = url+"/rx";
 export const insuranceUrl = url+"/insurance";
 export const pdUrl = url+"/pd";
+export const rxLedgerURL = url+"/rxledger";
+export const hackStatusURL = url+"/bcs";
